edc8b266324e6ed0e95b710124cd5c50d035329d
Justin <jtpank34@gmail.com>
2023-04-23 17:40:10 -0700

    charts also on teambuilder page per ethan

--- dk-app/src/routes/TeamBuilder.js
+++ dk-app/src/routes/TeamBuilder.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import '../styles/styles.css';
 import EntryField from '../components/EntryField';
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
+import { BarChart } from '../components/BarChart';
 class TeamBuilder extends React.Component {
     constructor(props){
         super(props);
@@ -9,6 +12,101 @@ class TeamBuilder extends React.Component {
         }
       }
     render() {
+        Chart.register(CategoryScale);
+        let captainMap = new Map();
+        let entries = this.props.numEntries; // 1 captain per lineup
+        for(let i = 0; i < entries; ++i)
+        {
+            const captain = this.props.allLineups[i]._captain;
+            if(captain != null && Object.keys(captain).length !== 0)  // captain is not empty and not null
+            {
+                if(captain.player_name != null)  //player name not null
+                {
+                    if(captainMap.has(captain.player_name))
+                    {
+                        let prevVal = captainMap.get(captain.player_name);
+                        captainMap.set(captain.player_name, prevVal + 1);
+                    }
+                    else
+                    {
+                        captainMap.set(captain.player_name,1);
+                    }
+                }
+            }
+        }
+        const captainData =
+        {
+            labels: [...captainMap.keys()], 
+            datasets: [
+            {
+                label: "Percent Exposed ",
+                data: [...captainMap.values()].map(val =>  ((val/(entries))*100.0)),
+            }
+            ]
+        }
+
+
+        let totalPlayerMap = new Map();
+        for(let i = 0; i < entries; ++i)
+        {
+            const lineup = this.props.allLineups[i];
+            if(lineup)
+            {
+                    if(lineup._utility)
+                    {
+                        const utilArray = lineup._utility;
+                        for(let j = 0; j < 5; ++j)
+                        {
+                            const utilPlayer = utilArray[j];
+                            if(utilPlayer != null && Object.keys(utilPlayer).length != 0)
+                            {
+                                if(utilPlayer.player_name != null)
+                                {
+                                    if(totalPlayerMap.has(utilPlayer.player_name))
+                                    {
+                                        let prevVal = totalPlayerMap.get(utilPlayer.player_name);
+                                        totalPlayerMap.set(utilPlayer.player_name, prevVal + 1);
+                                    }
+                                    else
+                                    {
+                                        totalPlayerMap.set(utilPlayer.player_name,1);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    if(lineup._captain)
+                    {
+                        const captain = this.props.allLineups[i]._captain;
+                        if(captain != null && Object.keys(captain).length !== 0)  // captain is not empty and not null
+                        {
+                            if(captain.player_name != null)  //player name not null
+                            {
+                                if(totalPlayerMap.has(captain.player_name))
+                                {
+                                    let prevVal = totalPlayerMap.get(captain.player_name);
+                                    totalPlayerMap.set(captain.player_name, prevVal + 1);
+                                }
+                                else
+                                {
+                                    totalPlayerMap.set(captain.player_name,1);
+                                }
+                            }
+                        }
+                    }
+
+            }
+        }
+        const utilityData =
+        {
+            labels: [...totalPlayerMap.keys()], 
+            datasets: [
+            {
+                label: "Percent Exposed ",
+                data: [...totalPlayerMap.values()].map(val => ((val/(entries))*100.0)),
+            }
+            ]
+        }
         let salaries_disp = "Salaries not uploaded!";
         let entries_disp = "Entries not uploaded!";
         if(this.props.isSalariesUploaded)
@@ -64,6 +162,16 @@ class TeamBuilder extends React.Component {
             failureDict={this.props.failureDict}
             ></EntryField>
             <div>
+            </div>
+            <div className='bar-chart-style'>
+                <BarChart 
+                chartData={captainData} 
+                labelName={"Captain"}/>
+
+                <BarChart 
+                chartData={utilityData}
+                labelName={"Total"} 
+                />
             </div>
         </div>
         );
